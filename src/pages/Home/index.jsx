7d24ee8f8c13e116480b4b6bdba70dd4d10a2fa0
cosmas <ezejioforcosmas@gmail.com>
2021-10-16 20:33:03 +0100

    testing

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,14 +3,14 @@ import style from "./home.module.scss";
 import hero from "../../assets/images/m-pack/m-pack2_o7d8mi_c_scale,w_1920.webp";
 import hero1 from "../../assets/images/m-pack/m-pack2_o7d8mi_c_scale,w_310.webp";
 import hero2 from "../../assets/images/m-pack/m-pack2_o7d8mi_c_scale,w_1177.webp";
-import weather from "../../assets/images/weather/weather_hrfd74_c_scale,w_730.webp";
-import weather1 from "../../assets/images/weather/weather_hrfd74_c_scale,w_310.webp";
-import blog from "../../assets/images/blog/blog_uui62c_c_scale,w_730.webp";
-import blog1 from "../../assets/images/blog/blog_uui62c_c_scale,w_310.webp";
-import ecommerce from "../../assets/images/ecommerce/ecommerce_clo8px_c_scale,w_730.webp";
-import ecommerce1 from "../../assets/images/ecommerce/ecommerce_clo8px_c_scale,w_310.webp";
-import ecommerce2 from "../../assets/images/ecommerce/ecommerce_clo8px_c_scale,w_684.webp";
-import ProductCard from "../../components/ProductCard/ProductCard";
+// import weather from "../../assets/images/weather/weather_hrfd74_c_scale,w_730.webp";
+// import weather1 from "../../assets/images/weather/weather_hrfd74_c_scale,w_310.webp";
+// import blog from "../../assets/images/blog/blog_uui62c_c_scale,w_730.webp";
+// import blog1 from "../../assets/images/blog/blog_uui62c_c_scale,w_310.webp";
+// import ecommerce from "../../assets/images/ecommerce/ecommerce_clo8px_c_scale,w_730.webp";
+// import ecommerce1 from "../../assets/images/ecommerce/ecommerce_clo8px_c_scale,w_310.webp";
+// import ecommerce2 from "../../assets/images/ecommerce/ecommerce_clo8px_c_scale,w_684.webp";
+// import ProductCard from "../../components/ProductCard/ProductCard";
 
 export default function MainHome() {
   return (
@@ -37,30 +37,30 @@ export default function MainHome() {
       </div>
       <section id="pack" className="main-container">
         <div className={style.packGrid}>
-          <ProductCard
+          {/* <ProductCard
             srcset={`${ecommerce1} 310w,${ecommerce2} 684w,${ecommerce} 730w`}
             src={ecommerce}
             to="/ecommerce/shop"
             title="Ecommerce"
-          />
-          <ProductCard
+          /> */}
+          {/* <ProductCard
             srcset={`${weather1} 310w,${weather} 730w`}
             src={weather}
             to="/weather"
             title="Weather"
-          />
-          <ProductCard
+          /> */}
+          {/* <ProductCard
             srcset={`${blog1} 310w,${blog} 730w,`}
             src={blog}
             to="/blog/posts"
             title="Blog"
-          />
-          <ProductCard
+          /> */}
+          {/* <ProductCard
             // srcset={`${blog1} 310w,${blog} 730w,`}
             src="https://via.placeholder.com/400x210"
             to="/"
             title="Coming Soon!"
-          />
+          /> */}
         </div>
       </section>
     </main>
